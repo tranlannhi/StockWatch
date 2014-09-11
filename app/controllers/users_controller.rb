2d64c3bdeced8a92b56e8af13c55daa973b586e4
tranlannhi@yahoo.com <tranlannhi@yahoo.com>
2014-09-11 11:28:39 -0700

    update user controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,5 +3,19 @@ class UsersController < ApplicationController
 	end
 
 	def create
+		@user = User.new(user_params)
+		if @user.save
+			flash[:notice] = "Welcome to the site!"
+			redirect to "/"
+		else
+			flash[:alert] = "There was a problem creating your account!"
+			redirect to :back
+		end
+	end
+
+	private
+
+	def user_params
+		params.require(:user).permit(:email, :password, :password_confirmation)
 	end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,9 +11,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 20140910192106) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "users", force: true do |t|
+    t.string   "email"
+    t.string   "password_hash"
+    t.string   "password_salt"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
 end
