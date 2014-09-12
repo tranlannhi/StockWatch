b028072eef2552b3cdfb119d6880cb73f3ef253d
tranlannhi@yahoo.com <tranlannhi@yahoo.com>
2014-09-11 21:36:23 -0700

    glyphions not working

--- config/application.rb
+++ config/application.rb
@@ -9,12 +9,14 @@ require "action_view/railtie"
 require "sprockets/railtie"
 # require "rails/test_unit/railtie"
 
+
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
 module StockWatch
   class Application < Rails::Application
+    config.assets.paths << "#{Rails}/vendor/assets/fonts"
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
